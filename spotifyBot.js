486fd7ebffa61e7d86b29d76245e122d644ed9cc
Maxwell Krause <maxwell.l.krause@gmail.com>
2018-08-12 16:20:57 -0400

    spotify responses working, time to shape for menu

--- spotifyBot.js
+++ spotifyBot.js
@@ -8,42 +8,61 @@ const { searchSpotify, playTrack } = require('./spotifyService');
 
 module.exports = botBuilder(async (message) => {
   const { originalRequest: slackResponse } = message;
-  console.log(slackResponse);
-  console.log('PLAY TRACK', await playTrack('1234'));
+  // eslint-disable-next-line camelcase
+  const { channel_id, response_url, user_id: user } = slackResponse;
+
+
+  //   { token: '4IyKwyYE3bgwZaQ5XF8kt8HJ',
+  // team_id: 'T31PFQN2F',
+  // team_domain: 'blumpkinclub',
+  // channel_id: 'C365JEZFY',
+  // channel_name: 'bot-testing',
+  // user_id: 'U3410GZMM',
+  // user_name: 'maxwellsmart84',
+  // command: '/remotecontrol',
+  // text: 'Arcade Fire, Suburbs',
+  // response_url: 'https://hooks.slack.com/commands/T31PFQN2F/416249902887/baayAAaRgiavIL0D03FsJ6F2',
+  // trigger_id: '415079950675.103797838083.e933247ecbeb17993fbc8c67a3126bb5' }
   // const slackWebClient = new WebClient(process.env.SPOTIFY_TOKEN);
   const [artist, track] = message.text.split(',');
 
   if (!artist || !track) {
     return 'Please enter an Artist and Track like this: Whitney Houston, I Wanna Dance With Somebody';
   }
-  const { tracks: { items: hits } = [] } = await searchSpotify(artist, track);
+  const { tracks: { items: hits } = [] } = await searchSpotify({ artist, track });
   if (hits.length === 0) {
     return 'Sorry we could not find the song you were looking for, try again';
   }
-  console.log('hits', hits);
+  console.log('SPOTIFY SEARCH RESPONSE', hits);
+  const slackRequest = {
+    channel_id,
+    response_url,
+    user,
+    attachments: [{}],
+  };
   return 'just the hits';
-  // const slackResponse = hits.map((data) => {
-  //   return {
-
-  //   }
-  // })
+  // const slackMenuData = hits.map(data => ({}));
 
   // const testResponse = spotifyTestData.map((data) => {
-  //   const { url: imageUrl } = data.album.images.find((image) =>  image.height === '64');
+  //   const { url: imageUrl } = data.album.images.find(image => image.height === '64');
   //   return {
-  //     image: imageUrl,
+  //     fallback: 'Something went wrong',
+  //     text: 'Select a song from the search results',
+  //     image_url: imageUrl,
   //     actions: [{
+  //       name: 'songList',
+  //       type: 'select',
   //       options: [{
   //         title: `${artists[0].name}, ${data.name}, ${album.name}`,
   //         value: {
-  //           uri:`${data.uri}`,
+  //           uri: `${data.uri}`,
   //           id: `${data.id}`,
-  //         }
-  //       }]
-  //     }]
+  //         },
+  //       }],
+  //     }],
 
-  //   }
-  // })
+  //   };
+  // });
 
 
   // return songForm
--- spotifyService.js
+++ spotifyService.js
@@ -2,30 +2,24 @@ const got = require('got');
 
 const BASE_SPOTIFY_REQ = got.extend({
   baseUrl: 'https://api.spotify.com',
-  query: { limit: 3 },
   json: true,
 });
 
 
-module.export = {
+module.exports = {
   async searchSpotify({ artist, track } = {}) {
-    const cleanParams = `artist:${encodeURIComponent(artist)}track:${encodeURIComponent(track)}`;
-    const uri = `/search?q=${cleanParams}`;
+    const cleanParams = `artist:${encodeURIComponent(artist.trim())}%20track:${encodeURIComponent(track.trim())}`;
+    const uri = `v1/search?&type=track&limit=3&q=${cleanParams}`;
     const { access_token: token } = await authSpotify();
     try {
-      const reply = await BASE_SPOTIFY_REQ.get(`/v1${uri}`, {
+      const { body: reply } = await BASE_SPOTIFY_REQ.get(uri, {
         uri,
         headers: { Authorization: `Bearer ${token}` },
         qsParseOptions: { encode: false },
-        qs: {
-          // q: params,
-          limit: 6,
-        },
-        json: true,
       });
       return reply;
     } catch (err) {
-      console.error('SEARCH SPOTIFY', err);
+      console.error('SEARCH SPOTIFY ERROR', err);
       throw new Error(err);
     }
   },
@@ -39,14 +33,15 @@ module.export = {
 async function authSpotify() {
   const authString = Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64');
   try {
-    const reply = await got.post('https://accounts.spotify.com/api/token', {
-      form: { grant_type: 'client_credentials' },
+    const { body: reply } = await got.post('https://accounts.spotify.com/api/token', {
+      form: true,
+      body: { grant_type: 'client_credentials' },
       headers: { Authorization: `Basic ${authString}` },
       json: true,
     });
     return reply;
   } catch (err) {
-    console.error('AUTH SPOTIFY', err);
+    console.error('AUTH SPOTIFY ERROR', err);
     throw new Error(err);
   }
 }
